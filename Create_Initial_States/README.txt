2153bc1feb47a6ee14fc8890a00a6239fc036c5f
Harsh Doshi <harsh.d2013@gmail.com>
2026-04-08 20:26:37 +0200

    Updated CIS execute.sh file with new paths and script arguments

--- Create_Initial_States/README.txt
+++ Create_Initial_States/README.txt
@@ -1,13 +1,13 @@
 This directory contains files and scripts used to create the initial states or configuration files from where segregation simulations can begin.
-The initial states are usually mixed states of two polymers confined in a cylinder. They are created using one of the four initialization procedure algorithms.
+The initial states are usually mixed states of two polymers confined in a cylinder. They are created using one of the initialization procedure algorithms.
 
 The b200/ and b500/ directories contain the files for 200 monomer and 500 monomer systems respectively.
 These directories contain a separate directory for each of the initialization procedure used: fene_recenter/ (COM-Recenter), fene_glued/ (Glued-Monomers), fene_ladder/ (Ladder-Like), and fene_mutual/ (Mutual-Attraction).
+The b200/ directory contains an additional directory inf_recenter/ which is a simulation of polymers in a nearly infinite cylinder.
 
 In order to create a mixed state for a particular architecture, the following procedure is followed:
-i) The basic input file is created using one of the multiple createInputFile.py or similar python scripts. These scripts create a barebones input script with Arc0 polymer(s).
-ii) The basic input file is modified to give an input file for an architecture with additional cross links. This is done via the script createInitialState.c in /cluster-data/initialization/fixing-bonding/.
-iii) The input file is then fed into a LAMMPS script such as shrink_relax.lammps to run a simulation. The state obtained at the end of the simulation is dumped and is the required mixed state.
+i) An initial state file is created for a particular polymer architecture which will be used to start the simulation.
+ii) The input file is then fed into a LAMMPS script such as shrink_relax.lammps to run a simulation. The state obtained at the end of the simulation is dumped and is the required mixed state.
 
 After the mixed states are created, they are tested to see if they are suitable to proceed to the segregation step. Currently these tests include:
 i) Check for concatenations across the two polymers and within each polymer
@@ -29,4 +29,13 @@ Example usage:
 <LAMMPS_EXEC> -in fene_recenter.lammps -var seed 532905 -var numberOfMonomers 200 -var run 01 -var angle1 0 -var angle2 0 -var finalRadius 3.12 -log shrink_log.lammps
 An initial_configuration.txt file like in Create_Initial_States/Scripts/ must be present in the directory where this script is run.
 This script will perform the simulation and write the snapshots of the system as a LAMMPS output file. It also stores the last snapshot in a special langevin_mixed_state_01.txt file which is used for the segregation simulation.
-The C and Python analysis files in Segregation/Scripts/ and Create_Initial_States/Scripts/ can be run manually to analyse the simulation output. Each of the scripts takes its own arguments/parameters which can be found out by simply running the script.
+
+The checks (ii) and (iii) are also performed automatically in execute.sh after the simulation has finished.
+This analysis involves running the C files present in Create_Initial_States/Scripts/. They can be compiled and run manually as well. Please ensure that the path to the base directory (Segregation-Kinetics-Cylinder-AC/) is passed as an include path using the -I option while compiling any script using gcc.
+The C scripts take some command line arguments, and the usage is explained when the compiled output is run without passing any arguments.
+
+Finally, the Python scripts in Create_Initial_States/Scripts/ can be run manually to plot the calculated monomer densities that were also shown in the paper.
+The scripts are prefixed with the word 'plot'. All these scripts take a few command line arguments to run properly. The usage is displayed when these scripts are run without passing any arguments.
+
+Once all the above steps are done, the user can move on to the Check_Concatenation/ folder if the concantenations have not already been checked.
+
--- Create_Initial_States/Scripts/execute.sh
+++ Create_Initial_States/Scripts/execute.sh
@@ -6,16 +6,18 @@ architecture=$2
 run=$3
 # NOTE: Please ensure that the Global_Scripts/System_File_Paths/system_file_paths.h config file has the appropriate (desired) special simulation set
 seed=$4
-special_simulation=$5 # Argument for running a particula initialization algorithm in its special folder
+special_simulation=$5 # Argument for running a particular initialization algorithm in its special folder
 
 # filePaths:
-SCRIPTS=/Global_Scripts/
-CREATE_INITIAL_STATES=/Create_Initial_States/
-SEGREGATION=/Segregation/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd $SCRIPT_DIR/../../ && pwd)/"
+SCRIPTS=${BASE_DIR}Global_Scripts/
+CREATE_INITIAL_STATES=${BASE_DIR}Create_Initial_States/
+SEGREGATION=${BASE_DIR}Segregation/
 
 if [ -z ${numberOfMonomers} ] || [ -z ${architecture} ] || [ -z ${run} ] || [ -z ${seed} ] || [ -z ${special_simulation} ] # -z flag checks whether the variable has zero length
 then
-        echo "Not enough arguments passed! Please pass number of monomers, the architecture, the run number, the two oritentation angles, the seed, and the initialization procedure folder name  while executing this script!"
+        echo "Not enough arguments passed! Please pass number of monomers, the architecture, the run number, the seed, and the initialization procedure folder name while executing this script!"
 #         echo "An optional argument for the special simulation identifier can be passed as well to store the files in a special directory"
         exit 1
 fi
@@ -41,23 +43,34 @@ else # The directory for the will be made directly under b<N>/ instead of in a <
 	fi
 	echo "No special simulation folder passed. Copying files in the default Create_Intial_States/b${numberOfMonomers} directory."
 fi
-shrink_relax_script=${CREATE_INITIAL_STATES}${folderPrefix}shrink_relax.lammps # The path to the shrink_relax LAMMPS script 
+
+# Creating the initial state from which the simulation will be run using the createInitialState.c script:
+cd ${CREATE_INITIAL_STATES}Scripts/
+gcc createInitialState.c -lm -std=gnu99 -o create.out -I ${BASE_DIR}
+./create.out ${numberOfMonomers} ${architecture} ${special_simulation} ${run}
+error=$?
+if ! [ ${error} -eq 0 ] # checking if the above process ended in an error
+then
+        echo "A problem was encountered while creating the initial state! Terminating."
+        exit ${error}
+fi
+
+# shrink_relax_script=${CREATE_INITIAL_STATES}${folderPrefix}shrink_relax.lammps # The path to the shrink_relax LAMMPS script 
+shrink_relax_script=${CREATE_INITIAL_STATES}Scripts/${special_simulation}.lammps # The path to the shrink_relax LAMMPS script 
 
 # Compiling the C scripts for analysis:
 runFolder=${CREATE_INITIAL_STATES}${folderPrefix}${architecture}/run${run}/ # storing the compiled executables separately for each run
 # Monomer distribution:
-cd ${NEW_SEGREGATION}Scripts/
-gcc sectional_monomer_density.c -lm -std=gnu99 -o ${runFolder}sect.out
-echo "The regions are read from the regions_config.h file in Harsh/Scripts/Config_Files/. Please ensure this information is correctly set!"
-gcc general_monomer_density.c -lm -std=gnu99 -o ${runFolder}ge.out
-gcc radial_monomer_density.c -lm -std=gnu99 -o ${runFolder}radial.out
+cd ${SEGREGATION}Scripts/
+echo "The regions are read from the regions_config.h file in Global_Scripts/Config_Files/. Please ensure this information is correctly set!"
+gcc general_monomer_density.c -lm -std=gnu99 -o ${runFolder}ge.out -I ${BASE_DIR}
+gcc radial_monomer_density.c -lm -std=gnu99 -o ${runFolder}radial.out -I ${BASE_DIR}
 # CoM Time series:
-gcc calculateCoMTimeSeries.c -lm -std=gnu99 -o ${runFolder}com.out
-# Bond distribution: not useful for FENE bonds
+gcc calculateCoMTimeSeries.c -lm -std=gnu99 -o ${runFolder}com.out -I ${BASE_DIR}
+
 cd ${CREATE_INITIAL_STATES}Scripts/
-# gcc calculateBondEnergyDistribution.c -lm -std=gnu99 -o ${runFolder}energy.out
-# Pair correlation:
-gcc pair_correlation.c -lm -std=gnu99 -o ${runFolder}pair.out
+gcc sectional_monomer_density.c -lm -std=gnu99 -o ${runFolder}sect.out -I ${BASE_DIR}
+
 
 # Reading the diameter from Diameters.csv database using bash functions defined in another script
 . ${SCRIPTS}bash_functions.sh # 'Importing' the bash functions file
@@ -79,7 +92,12 @@ echo "Running shrink-relax LAMMPS script"
 echo "Seed = ${seed}"
 
 # Running the shrink-relax simulation:
-LAMMPS_EXEC=`which lmp_gcc` # Replace the name of the LAMMPS executable for your installation if lmp_gcc is not found
+# LAMMPS_EXEC=`which lmp` # Replace the name of the LAMMPS executable for your installation if lmp is not found
+LAMMPS_EXEC=/usr/bin/lmp
+if [ -z ${LAMMPS_EXEC} ]; then
+	echo "LAMMPS executable not found in PATH! Please ensure LAMMPS is installed and the executable is in the PATH, or update the LAMMPS_EXEC variable in this script to point to the executable."
+	exit 1
+fi
 # Shrink-Relax:
 ${LAMMPS_EXEC} -in ${shrink_relax_script} -var seed ${seed} -var numberOfMonomers ${numberOfMonomers} -var run $(printf %02d ${run}) -var angle1 ${angle1} -var angle2 ${angle2} -var finalRadius ${final_radius} -log shrink_log.lammps
 error=$?
@@ -93,43 +111,37 @@ fi
 echo ""
 echo "Monomer Distribution:"
 mkdir -p monomer_distribution
-mkdir -p bond_energy
-mkdir -p pair_correlation
 # Monomer distribution:
 cd ${SEGREGATION}Scripts/ # going to Segregation/Scripts
 # Ensure that the Global_Scripts/Utility/Section.h file has the desired sections
 
+if [[ ${special_simulation} == *"inf"* ]]; then # Checking for the presence of "inf" indicating simulation in an infinite cylinder
+	axisLength="" # Empty
+else
+	axisLength=$( bc <<< "scale=2; 10 * ${final_radius}")
+fi
+
 binWidth=0.2
-${runFolder}sect.out ${numberOfMonomers} ${architecture} ${run} ${binWidth}
+${runFolder}sect.out ${numberOfMonomers} ${architecture} ${run} ${binWidth} ${special_simulation} ${axisLength}
 rm ${runFolder}sect.out # removing the executable
 
 # calculating regionwise monomer distribution: (regions of different polymer)
 echo ""
 echo "Regional Monomer Distribution:"
 binWidth=0.1 # updating binWidth for radial distribution
-axisLength=$( bc <<< "scale=2; 10 * ${final_radius}")
-${runFolder}ge.out ${numberOfMonomers} ${architecture} ${run} Create_Initial_States 0 ${axisLength}
+${runFolder}ge.out ${numberOfMonomers} ${architecture} ${run} Create_Initial_States ${special_simulation} 0 ${axisLength}
 rm ${runFolder}ge.out
 
 # calculating radial distribution:
 echo ""
 echo "Radial Monomer Distribution:"
-${runFolder}radial.out ${numberOfMonomers} ${architecture} ${run} ${binWidth} Create_Initial_States
+${runFolder}radial.out ${numberOfMonomers} ${architecture} ${run} ${binWidth} ${special_simulation} Create_Initial_States
 rm ${runFolder}radial.out
 
 # Region wise CoM Time series:
 echo ""
 echo "Region wise CoM Time Series:"
-${runFolder}com.out ${numberOfMonomers} ${architecture} ${run} Create_Initial_States
+${runFolder}com.out ${numberOfMonomers} ${architecture} ${run} ${special_simulation} Create_Initial_States
 rm ${runFolder}com.out
-
-# calculating the pair_correlation functions:
-echo ""
-echo "Pair Correlation Function:"
-range=$( ceil ${final_radius} ) # ceil() function imported from bash functions; range is maximum distance up to which the pair_correlation is to be calculated
-${runFolder}pair.out ${numberOfMonomers} ${architecture} ${run} ${range} ${binWidth} 1 1
-# The second last argument 1 is a flag to indicate that the pair correlations should be calculated separately
-# The last argument 1 is a flag to indicate that the bonded neighbours should be ignored while calculating the pair correlation components
-rm ${runFolder}pair.out
 
 echo "Done"
